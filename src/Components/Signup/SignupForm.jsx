d216149a4783791be437ece1c9bc6a5141dd494e
Rona <longhairshortcode@gmail.com>
2024-02-07 22:20:41 -0800

    closest possible

--- src/Components/Signup/SignupForm.jsx
+++ src/Components/Signup/SignupForm.jsx
@@ -4,13 +4,16 @@ import style from "./SignupForm.module.css"
 function SignupForm() {
   return (
     <div className={style.container}>
+
       {/* <h2 className={style.formH2}>Tell us about you</h2> */}
-      <form className={style.form}>
+      <form className={style.formContainer}>
         
         
         <div className={`${style['form1']} ${style['formsBoth']}`}>
+          
           <input type="text" className={style.firstName} placeholder='First name*'/>
           <input type="text" className={style.lastName} placeholder='Last name*'/>
+
           <input type="text" className={style.practiceName} placeholder='Practice name*'/>
           <input type="email" className={style.emailAddress} placeholder='Email address*'/>
           <input type="tel" className={style.phone}placeholder='Phone*'/>
@@ -18,14 +21,14 @@ function SignupForm() {
         </div>
         
         <div className={`${style['form2']} ${style['formsBoth']}`}>
-          <select>
+          <select className={style.selectPlan}>
             <option>Select Plan</option>
             <option>Plan 1</option>
             <option>Plan 2</option>
             <option>Plan 3</option>
           </select>
-          <input/>
-          <textarea></textarea>
+          <input type="text" className={style.addr} placeholder="Enter your address"/>
+          <textarea className={style.comments} placeholder="Got a question? Feel free to type it here!"></textarea>
         </div>
   
       </form>
