ec94678315e2d4656d1d7b56e72821e1435d7c96
Rona <longhairshortcode@gmail.com>
2024-02-16 19:52:50 -0800

    completed all 3 versions of input event handling in signup

--- src/Components/Signup/SignupForm.jsx
+++ src/Components/Signup/SignupForm.jsx
@@ -4,10 +4,55 @@ import { useEffect } from "react"
 
 function SignupForm() {
 
+            //CREATE userData (type = object w/ all the "name" props of all the inputs) state/setter
+
+  // Q- Clarify & confirm: in the MID, when triggered by the onChange, the setter function for
+  //the userData variable(state) (which type is an obj) is updating one 
+  //of it's properties' state (), or is it actually "updating one of it's props's value"? Are
+  //each prop (which are the name/id in the input) just a prop or also a state or just a state?
+  const [userData, setUserData] = useState({
+    // all the inputs by unique name(like ID)
+    firstName: "",
+    lastName: "",
+    practiceName: "",
+    emailAddress: "",
+    tel: "",
+    url: "",
+    selectPlan: "",
+    homeAddr: "",
+    comment: "",
+  })
+          //CREATE handleChange handler function that extracts name/value from event, and then updates states 
+          //with them vis setUserData setter 
+
+  // For the ADVANCED, make a handleChange function which 1) grabs/exposes name and value props from event 
+  //2) then uses the setter function for the userData object to update the value with the props grabbed above 
+  //Q - what EXACTLY is the e below, it's an event, but what is the actual event mean?
+  function handleChange(e){
+  const {name, value} = e.target
+  //so since it is object, [name]:value is how to rename it since [name] is name in the
+  //object and value is the value in e.target, and can't set the name in the object to the value in the event a 
+  //different way?
+  setUserData(prev=>({...prev, [name]:value}))
+}
+
+// function handleChange(e){
+//   const {name, value} = e.target
+//   setUserData(prev=>({...prev, [name]:value}))
+// }
+
+// function handleChange(e){
+//   const [name, value] = e.target
+//   setUserData(prev=>({...prev, [name]:value}))
+// }
+
+// Q - need this for all 3 levels? I think so since need on all forms?
   function handleSubmit(e){
     e.preventDefault() 
   }
 
+   //in the BASIC, when triggered by the onChange, the setter 
+  // function for each is alone is updating each variable's state here seperately 1 by 1
   const [firstName, setFirstName] = useState("")
   const [lastName, setLastName] = useState("")
   const [practiceName, setPracticeName] = useState("")
@@ -19,22 +64,23 @@ function SignupForm() {
   const [comments, setComments] = useState("")
   
 
-   useEffect(()=>{
-   console.log(emailAddress)
-   console.log(firstName)
-  }, [emailAddress, firstName])
+  //Q do i need this at all, and when would I need it?
+  //  useEffect(()=>{
+  //  console.log(emailAddress)
+  //  console.log(firstName)
+  // }, [emailAddress, firstName])
   
 
 
   return (
     <div className={style.container}>
 
-      <form onSubmit={handleSubmit} className={style.formContainer}>
-        <h2 className={style.formH2}>Tell us about you</h2>        
+      <form onSubmit={handleSubmit} className={style.formContainer1}>
+        <h2 className={style.formH2}>Tell us about you: Basic</h2>        
         
         <div className={style.containerBelowH2}>
           <div className={`${style['form1']} ${style['formsBoth']}`}>
-          
+          {/* are all 3 types of inputs jsx or js or react? */}
             <input type="text" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} required className={style.firstName} placeholder='First name&lowast;'/>
             <input type="text" name="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} required className={style.lastName} placeholder='Last name&lowast;'/>
          
@@ -45,6 +91,7 @@ function SignupForm() {
           </div>
         
           <div className={`${style['form2']} ${style['formsBoth']}`}>
+            {/* is it okay this one doesn't have a name? */}
             <select value={plan} onChange={(e) => setPlan(e.target.value)} className={style.selectPlan}>
               <option className={style.selectPlanShow}>Select Plan&lowast;</option>
               <option>Plan 1</option>
@@ -57,6 +104,185 @@ function SignupForm() {
         </div>
         <button type="submit" className={style.submitBut}>Submit Form</button>
       </form>
+
+
+      <form onSubmit={handleSubmit} className={style.formContainer2}>
+        <h2 className={style.formH2}>Tell us about you: Mid</h2>        
+        
+        <div className={style.containerBelowH2}>
+          <div className={`${style['form1']} ${style['formsBoth']}`}>
+          {/* how do I correctly organize input structure for each of the 3? */}
+            <input 
+              type="text" 
+              name="firstName" 
+              value={firstName} 
+              onChange={(e) => setUserData(prev=>({...prev, firstName: e.target.value}))} 
+              required 
+              className={style.firstName} 
+              placeholder='First name&lowast;'/>
+            <input 
+            type="text" 
+            name="lastName" 
+            value={lastName} 
+            onChange={(e)=>setUserData(prev=>({...prev, lastName:e.target.value}))} 
+            required 
+            className={style.lastName} 
+            placeholder='Last name&lowast;'/>
+         
+            <input 
+              type="text" 
+              name="practiceName" 
+              value={practiceName} 
+              onChange={(e)=>setUserData(prev=>({...prev, lastName : e.target.value}))}
+              required 
+              className={style.practiceName} 
+              placeholder='Practice name&lowast;'/>
+            <input 
+              type="email" 
+              name="emailAddress" 
+              value={emailAddress} 
+              onChange={(e)=>setUserData(prev=>({...prev, emailAddress : e.target.value}))} 
+              required 
+              className={style.emailAddress} 
+              placeholder='Email address&lowast;'/>
+            <input 
+              type="tel" 
+              name="tel" 
+              value={tel} 
+              onChange={(e)=>setUserData(prev=>({...prev, tel : e.target.value}))}
+              required 
+              className={style.phone}
+              placeholder='Phone&lowast;'/>
+            <input 
+              type="url" 
+              name="url" 
+              value={url} 
+              onChange={(e)=>setUserData(prev=>({...prev, url:e.target.value}))}
+              required 
+              className={style.website}
+              placeholder='Website'/>
+          </div>
+        
+          <div className={`${style['form2']} ${style['formsBoth']}`}>
+            <select 
+            // Q/Clarification: this one didn't have a name, accidentally forgot or doesn't have name? Will add one for now, esp
+            //since i havea plan state variable at the top
+              name="plan"
+              value={plan} 
+              onChange={(e)=>setUserData(prev=>({...prev, plan:e.target.value}))}
+              className={style.selectPlan}>
+                <option className={style.selectPlanShow}>Select Plan&lowast;</option>
+                <option>Plan 1</option>
+                <option>Plan 2</option>
+                <option>Plan 3</option>
+            </select>
+            <input 
+              type="text" 
+              name="homeAddr" 
+              value={homeAddr} 
+              onChange={(e)=>setUserData(prev({...prev, homeAddr:e.target.value}))}
+              required 
+              className={style.addr} 
+              placeholder="Enter your address&lowast;"/>
+            <textarea 
+              name="comments" 
+              value={comments} 
+              onChange={(e)=>setUserData(prev=>({...prev, comments:e.target.value}))}
+              required 
+              className={style.commentsSection} 
+              placeholder="Got a question? Feel free to type it here!"></textarea>
+          </div>
+        </div>
+        <button type="submit" className={style.submitBut}>Submit Form</button>
+      </form>
+
+      <form onSubmit={handleSubmit} className={style.formContainer3}>
+        <h2 className={style.formH2}>Tell us about you: Advanced</h2>        
+        
+        <div className={style.containerBelowH2}>
+          <div className={`${style['form1']} ${style['formsBoth']}`}>
+          
+            <input 
+              type="text" 
+              name="firstName" 
+              value={firstName} 
+              onChange={handleChange} 
+              required 
+              className={style.firstName} 
+              placeholder='First name&lowast;'/>
+            <input 
+              type="text" 
+              name="lastName" 
+              value={lastName} 
+              onChange={handleChange}  
+              required 
+              className={style.lastName} 
+              placeholder='Last name&lowast;'/>
+         
+            <input 
+              type="text" 
+              name="practiceName" 
+              value={practiceName} 
+              onChange={handleChange}
+              required 
+              className={style.practiceName} 
+              placeholder='Practice name&lowast;'/>
+            <input 
+              type="email" 
+              name="emailAddress" 
+              value={emailAddress} 
+              onChange={handleChange} 
+              required 
+              className={style.emailAddress} 
+              placeholder='Email address&lowast;'/>
+            <input 
+              type="tel" 
+              name="tel" 
+              value={tel} 
+              onChange={handleChange} 
+              required 
+              className={style.phone}
+              placeholder='Phone&lowast;'/>
+              <input 
+              type="url" 
+              name="url" 
+              value={url} 
+              onChange={handleChange} 
+              required 
+              className={style.website}placeholder='Website'/>
+          </div>
+        
+          <div className={`${style['form2']} ${style['formsBoth']}`}>
+            <select 
+              name="plan"
+              value={plan} 
+              onChange={handleChange}
+              className={style.selectPlan}>
+                <option className={style.selectPlanShow}>Select Plan&lowast;</option>
+                <option>Plan 1</option>
+                <option>Plan 2</option>
+                <option>Plan 3</option>
+            </select>
+            <input 
+              type="text" 
+              name="homeAddr" 
+              value={homeAddr} 
+              onChange={handleChange}
+              required 
+              className={style.addr}
+              placeholder="Enter your address&lowast;"/>
+            <textarea 
+              name="comments" 
+              value={comments} 
+              onChange={handleChange} 
+              required 
+              className={style.commentsSection} 
+              placeholder="Got a question? Feel free to type it here!"></textarea>
+          </div>
+        </div>
+        <button type="submit" className={style.submitBut}>Submit Form</button>
+      </form>
+
     </div>
   )
 }
--- src/pages/SignupForm2.jsx
+++ src/pages/SignupForm2.jsx
@@ -5,19 +5,45 @@ import { useState } from "react"
 
 function SignupForm2() {
   
-    function handleSubmit(e){
-        e.preventDefault()
-    }
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    practiceName: "",
+    emailAddress: "",
+    tel: "",
+    url: "",
+    selectPlan: "",
+    homeAddr: "",
+    comment: "",
+  })
+    // this function is an event handler for input change events
+  function handleChange(e){
+    // destucturing (get/grab/expose) the name and value props from the event(the active input) target(the input you pressed)
+    const {name, value} = e.target
+    // reassigning of the user state
+    setUserData(prev => ({
+      // spread the previous data to store it in memory
+      ...prev, 
+      [name] : value // update the specifc field indicated by name (name is the id of the input) with the new value
+    }))
+  }
 
-    const [firstName, setFirstName] = useState("") 
-    const [lastName, setLastName] = useState("")
-    const [practiceName, setPracticeName] = useState("")
-    const [emailAddress, setEmailAddress] = useState("")
-    const [tel, setTel] = useState("")
-    const [url, setUrl] = useState("")
-    const [selectPlan, setSelectPlan] = useState("")
-    const [homeAddr, setHomeAddr] = useState("")
-    const [comment, setComment] = useState("") 
+  function handleSubmit(e){
+        e.preventDefault()
+   }
+  
+
+
+
+    // const [firstName, setFirstName] = useState("") 
+    // const [lastName, setLastName] = useState("")
+    // const [practiceName, setPracticeName] = useState("")
+    // const [emailAddress, setEmailAddress] = useState("")
+    // const [tel, setTel] = useState("")
+    // const [url, setUrl] = useState("")
+    // const [selectPlan, setSelectPlan] = useState("")
+    // const [homeAddr, setHomeAddr] = useState("")
+    // const [comment, setComment] = useState("") 
 
   return (
     <div className={style.container}>
@@ -26,24 +52,82 @@ function SignupForm2() {
         <div className={style.containerOfBothForms}>
         <div className={style.formLeft}>
           <div className={style.firstLastContainer}>
-            <input className={style.firstName}  type="text" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} required placeholder="First Name&lowast;"/>
-            <input className={style.lastName} type="text" name="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} required placeholder="Last Name&lowast;"/>    
+            <input 
+                className={style.firstName}  
+                type="text" 
+                name="firstName" 
+                value={userData.firstName} 
+                onChange={handleChange}
+                required 
+                placeholder="First Name&lowast;"/>
+            
+            <input 
+                className={style.lastName} 
+                type="text" 
+                name="lastName" 
+                value={userData.lastName} 
+                onChange={(e) => setUserData(prev=>({...prev, lastName: e.target.value}))} 
+                required 
+                placeholder="Last Name&lowast;"/>    
           </div>
-            <input className={style.practiceName} type="text" name="practiceName" value={practiceName} onChange={(e) => setPracticeName(e.target.value)} required placeholder="Practice name&lowast;"/>
-            <input className={style.emailAddress} type="email" name="emailAddress" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} required placeholder="Email address&lowast;"/>
-            <input className={style.tel} type="tel" name="tel" value={tel} onChange={(e)=>setTel(e.target.value)} required placeholder="Phone&lowast;"/>
-            <input className={style.url} type="url" name="url" value={url} onChange={(e)=>setUrl(e.target.value)} placeholder="Website"/>
+            <input 
+                className={style.practiceName} 
+                type="text" 
+                name="practiceName" 
+                value={userData.practiceName} 
+                onChange={(e) => setUserData(prev=>({...prev, practiceName: e.target.value}))}
+                required 
+                placeholder="Practice name&lowast;"/>
+            <input 
+                className={style.emailAddress} 
+                type="email" 
+                name="emailAddress" 
+                value={userData.emailAddress} 
+                onChange={(e) => setUserData(prev=>({...prev, emailAddress: e.target.value}))}
+                required 
+                placeholder="Email address&lowast;"/>
+            <input 
+                className={style.tel} 
+                type="tel" 
+                name="tel" 
+                value={userData.tel} 
+                onChange={(e)=>setUserData(prev=>({...prev, tel: e.target.value}))}
+                required 
+                placeholder="Phone&lowast;"/>
+            <input 
+                className={style.url} 
+                type="url" 
+                name="url" 
+                value={userData.url} 
+                onChange={(e)=>setUserData(prev=>({...prev, url: e.target.value}))}
+                placeholder="Website"/>
         </div>
            
         <div className={style.formRight}> 
-            <select className={style.selectPlan} value={selectPlan} onChange={(e)=>(setSelectPlan(e.target.value))} required>
-                <option className={style.selectPlanShown}>Select Plan&lowast;</option>
-                <option>Plan 1</option>
-                <option>Plan 2</option>
-                <option>Plan 3</option>
+            <select 
+                className={style.selectPlan} 
+                value={userData.selectPlan} 
+                onChange={(e)=>setUserData(prev=>({...prev, selectPlan:e.target.value}))}
+                required>
+                  <option className={style.selectPlanShown}>Select Plan&lowast;</option>
+                  <option>Plan 1</option>
+                  <option>Plan 2</option>
+                  <option>Plan 3</option>
             </select>
-            <input className={style.homeAddr} type="text" name="homeAddr" value={homeAddr} onChange={(e)=>setHomeAddr(e.target.value)} required placeholder="Enter your address&lowast;"/>
-            <textarea className={style.comment} name="comment" value={comment} onChange={(e)=>setComment(e.target.value)} placeholder="Got a question? Feel free to type it here!"></textarea>
+            <input 
+              className={style.homeAddr} 
+              type="text" 
+              name="homeAddr" 
+              value={userData.homeAddr} 
+              onChange={(e)=>setUserData(prev=>({...prev, homeAddr: e.target.value}))}
+              required 
+              placeholder="Enter your address&lowast;"/>
+            <textarea 
+              className={style.comment} 
+              name="comment" 
+              value={userData.comment} 
+              onChange={(e)=>setUserData(prev=>({...prev, comment:e.target.value}))}
+              placeholder="Got a question? Feel free to type it here!"></textarea>
         </div>
         </div>  
         <div>
