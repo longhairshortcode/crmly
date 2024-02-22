aa4d65d5bc850d74b2b1b72c58ef4674d5789bc1
Rona <longhairshortcode@gmail.com>
2024-02-22 10:52:30 -0800

    class work

--- src/pages/APIRequest.jsx
+++ src/pages/APIRequest.jsx
@@ -1,15 +1,28 @@
 // making an API request on click of an ordinary button (not a form button)
 import axios from "axios"
+import {useEffect, useState} from "react"
 
 function APIRequest(){
  
+    const [posts, setPosts] = useState([])
+    const allPosts = posts.map((post)=> (
+        <div style={{marginBottom: "10px", backgroundColor: "red"}}>
+            <h2>Title: {post.title}</h2>
+            <p>Body: {post.body}</p>    
+        </div>
+    )) 
+    useEffect(()=>{
+        console.log(posts)
+    }, [])
+
    async function myRequestFunction(){
         try{ 
             const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
-                console.log(response.status)
-                // if (response.status == 200){
-                //     //
-                // }
+                console.log(response.data)
+                if (response.status == 200){
+                    setPosts(response.data)
+                    
+                 }
         }catch(error){
             console.log(error)
         }
@@ -17,7 +30,9 @@ function APIRequest(){
 
     return(
         <div>
-            <button onClick={myRequestFunction}>Get Data</button>
+            {allPosts}
+            {/* <button onClick={myRequestFunction}>Get Data</button> */}
+            
         </div>
     )
 }
